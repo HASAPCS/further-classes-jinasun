a51cb3bb708e98d9b50bbf131922133f076af601
jinasun <ginasuns60916@gmail.com>
2023-11-01 12:15:02 +0800

    mjk

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
     private List<Product> products;
 
     public ShoppingCart() {
+        this.products = new ArrayList<>();
         // TODO: Initialize the list of products
     }
 
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    
     // TODO: Implement methods to add a product, remove a product, and calculate the total cost
 }
