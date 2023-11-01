69e3357bb27b390901b41a1350a24e9e0627e20d
jinasun <ginasuns60916@gmail.com>
2023-11-01 12:14:29 +0800

    fghjk

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -8,7 +8,42 @@ public class Employee {
 
     public Employee(String name, int id, double salary, String department) {
         // TODO: Initialize the attributes
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.department = department;
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
     // TODO: Implement getters, setters, and any other methods you find necessary
 }
