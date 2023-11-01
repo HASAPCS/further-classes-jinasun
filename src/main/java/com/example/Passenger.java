e519e04063df93f871dc546cc1d5ae72353713a3
jinasun <ginasuns60916@gmail.com>
2023-11-01 12:14:49 +0800

    tyui

--- src/main/java/com/example/Passenger.java
+++ src/main/java/com/example/Passenger.java
@@ -5,8 +5,25 @@ public class Passenger {
     private String passportNumber;
 
     public Passenger(String name, String passportNumber) {
+        this.name = name;
+        this.passportNumber = passportNumber;
         // TODO: Initialize the attributes
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public String getPassportNumber(){
+        return passportNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPassportNumber(String passportNumber){
+        this.passportNumber = passportNumber;
+    }
     // TODO: Implement getters, setters, and any other methods you find necessary
 }
