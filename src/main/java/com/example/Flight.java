fa1d35da7d63860f8e56caa114d2f06ae8c434f0
jinasun <ginasuns60916@gmail.com>
2023-11-01 12:14:36 +0800

    vbnm

--- src/main/java/com/example/Flight.java
+++ src/main/java/com/example/Flight.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flight {
@@ -9,8 +10,34 @@ public class Flight {
     private List<Passenger> passengers;
 
     public Flight(String flightNumber, String destination, String origin) {
-        // TODO: Initialize the attributes
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.origin = origin;
+        this.passengers = new ArrayList<Passenger>();
     }
 
+    public void bookFlight(Passenger passenger) {
+        passengers.add(passenger);
+    }
+
+    public void cancelBooking(Passenger passenger) {
+        passengers.remove(passenger);
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
     // TODO: Implement methods to book a flight for a passenger and cancel a booking
 }
